2c48ec3901e28275ad5a7caf42d8720953fe14e2
david-beltrang <estedavid0104@gmail.com>
2026-04-05 20:28:12 -0500

    Remove unnecessary comments in EventoEspira class for clarity

--- traffic_project/PC2/dtos/evento_espira.py
+++ traffic_project/PC2/dtos/evento_espira.py
@@ -21,7 +21,7 @@ class EventoEspira(EventoSensor):
             sensor_id = data["sensor_id"],
             interseccion_id = data["interseccion"],
             calle_id = data.get("calle_id") or data.get("calle", ""),
-            timestamp = data["timestamp_inicio"], # <--- ESTO ES VITAL PARA EL PADRE
+            timestamp = data["timestamp_inicio"],
             vehiculos_contados = int(data["vehiculos_contados"]),
             intervalo_s = int(data["intervalo_segundos"]),
             timestamp_fin = data.get("timestamp_fin") # <--- EXTRA
@@ -33,7 +33,7 @@ class EventoEspira(EventoSensor):
             "tipo_sensor": "espira_inductiva",
             "vehiculos_contados": self.vehiculos_contados,
             "intervalo_s": self.intervalo_s,
-            "timestamp_fin": self.timestamp_fin # Añadimos el fin al log de la DB
+            "timestamp_fin": self.timestamp_fin
         }
 
     # Validar que los datos del evento sean correctos
